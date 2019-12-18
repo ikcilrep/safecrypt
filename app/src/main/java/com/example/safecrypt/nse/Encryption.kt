946d33b4c1ca4ad82598277675a6184a0b9937ef
ikcilrep <frajerzycki@protonmail.com>
2019-12-18 19:24:49 +0100

    Rename IV to iv

--- app/src/main/java/com/example/safecrypt/nse/Encryption.kt
+++ app/src/main/java/com/example/safecrypt/nse/Encryption.kt
@@ -5,12 +5,12 @@ import java.math.BigInteger
 fun encrypt(data: ByteArray, key: BigInteger, salt: ByteArray): Pair<LongArray, ByteArray> {
     val shiftedData = data.shiftRightBits(key)
     val derivedKey = deriveKey(data.size, key, salt)
-    val IV = generateIV(shiftedData.size, shiftedData, derivedKey)
+    val iv = generateIV(shiftedData.size, shiftedData, derivedKey)
     val result =
         shiftedData * derivedKey.dotProduct(derivedKey) - derivedKey * derivedKey.dotProduct(
-            shiftedData - IV
+            shiftedData - iv
         ) * 2
-    return Pair(result, IV)
+    return Pair(result, iv)
 }
 
 
