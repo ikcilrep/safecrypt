d8b934cd15bf16ab15f798311ac0341499f98dce
ikcilrep <frajerzycki@protonmail.com>
2019-12-18 18:44:13 +0100

    Change type of ciphertext in decryption

--- app/src/main/java/com/example/safecrypt/nse/Encryption.kt
+++ app/src/main/java/com/example/safecrypt/nse/Encryption.kt
@@ -3,11 +3,15 @@ package com.example.safecrypt.nse
 import java.math.BigInteger
 
 fun encrypt(data: ByteArray, key: BigInteger, salt: ByteArray): Pair<LongArray, ByteArray> {
-    val rightShifted = data.shiftRightBits(key)
+    /*val rightShifted = data.shiftRightBits(key)
     val derivedKey = deriveKey(data.size, key, salt)
     val IV = generateIV(rightShifted.size, rightShifted, derivedKey)
-    //val result = rightShifted * derivedKey.dotProduct(derivedKey) - derivedKey*derivedKey.dotProduct(rightShifted - IV)*2
+    val result = rightShifted * derivedKey.dotProduct(derivedKey) - derivedKey*derivedKey.dotProduct(rightShifted - IV)*2
+
+     */
+    TODO()
 }
 
-fun decrypt(data: Array<Long>, key: BigInteger, iv: ByteArray, salt: ByteArray): ByteArray =
+
+fun decrypt(data: LongArray, key: BigInteger, iv: ByteArray, salt: ByteArray): ByteArray =
     TODO()
